4bb2c8dbbbacc6afc4f8f12594c7ce3cdf6c310b
Carsten <carsten.ringe@web.de>
2011-02-24 17:59:55 +0100

    Added templating via Enlive.

--- project.clj
+++ project.clj
@@ -2,8 +2,9 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
+                 [enlive "1.0.0-SNAPSHOT"]
                  [compojure "0.6.0-RC4"]
-		 [ring/ring-jetty-adapter "0.3.5"]]
+                 [ring/ring-jetty-adapter "0.3.5"]]
   :dev-dependencies [[lein-ring "0.3.2"]]
   :ring {:handler CmpjrAjaxTest.app/app})
 
--- src/CmpjrAjaxTest/app.clj
+++ src/CmpjrAjaxTest/app.clj
@@ -1,22 +1,27 @@
 (ns CmpjrAjaxTest.app
   (:use compojure.core
-	clojure.contrib.json
-	ring.adapter.jetty
-	CmpjrAjaxTest.core)
+        clojure.contrib.json
+        net.cgrand.enlive-html
+        ring.adapter.jetty
+        CmpjrAjaxTest.core)
   (:require [compojure.route :as route]
-            [compojure.handler :as handler]))
+    [compojure.handler :as handler]))
+
+(deftemplate index "resources/test.html"
+  [cmap]
+  [:span#msg] (content (:msg cmap)))
 
 (defroutes main-routes
-  (GET "/" [] "<h1>bar!</h1>")
+  (GET "/" [] (index {:msg "Hello World!"}))
+  (GET "/msg/:msg" [msg] (index {:msg msg}))
   (GET "/json" [] 
-       {:headers {"Content-Type" "text/html"}
-       :body (json-str {:foo "foo", :bar "bar"})})
+    {:headers {"Content-Type" "text/html"}
+     :body (json-str {:foo "foo", :bar "bar"})})
   (route/resources "/")
   (route/not-found "Page not found"))
 
 (def app
-    (handler/site main-routes))
+  (handler/site main-routes))
 
 (defn dev []
   (run-jetty #'app {:port 8080}))
-
